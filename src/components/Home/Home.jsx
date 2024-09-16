05ad411bf5b6961d27d1a8b4a3f22ecd5be0b0e6
Nucleo Fusion <lakshit.singh.mail@gmail.com>
2024-09-16 21:45:42 +0530

    Initial Commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import styles from "./Home.module.css";
+import bgimg from "../../images/1338700 (1).png";
+import wave from "../../images/wp3469892.webp";
+import $ from "jquery";
+
+export default function Home() {
+  useEffect(() => {
+    $("body").css("overflow", "hidden");
+  });
+
+  return (
+    <div className={styles.HomeDiv}>
+      <img src={bgimg} />
+      <img src={wave} className={styles.wave} />
+
+      <div className={styles.navbar}>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+
+      <div>
+        <h3 className="oswald">Lapis Presents</h3>
+        <h1 className="oswald">FootballAPI</h1>
+      </div>
+
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          sint aspernatur temporibus, odio minima impedit veniam exercitationem
+          quidem, laborum, ab id ullam? Provident vero enim qui asperiores
+          perspiciatis vitae libero!
+        </p>
+      </div>
+    </div>
+  );
+}
