e0695014583626d6fd2a3f4a133e517f00d39dfa
Nucleo Fusion <lakshit.singh.mail@gmail.com>
2024-09-21 18:58:56 +0530

    Adding HomePage Complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import styles from "./Home.module.css";
 import bgimg from "../../images/1338700 (1).png";
 import wave from "../../images/wp3469892.webp";
 import $ from "jquery";
+import Navbar from "../Navbar/Navbar";
 
 export default function Home() {
   useEffect(() => {
@@ -10,29 +11,32 @@ export default function Home() {
   });
 
   return (
-    <div className={styles.HomeDiv}>
-      <img src={bgimg} />
-      <img src={wave} className={styles.wave} />
+    <>
+      <Navbar />
+      <div className={styles.HomeDiv}>
+        <img src={bgimg} />
+        <img src={wave} className={styles.wave} />
 
-      <div className={styles.navbar}>
-        <ul>
-          <li></li>
-        </ul>
-      </div>
+        <div className={styles.navbar}>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
 
-      <div>
-        <h3 className="oswald">Lapis Presents</h3>
-        <h1 className="oswald">FootballAPI</h1>
-      </div>
+        <div>
+          <h3 className="bebas-neue-regular">Lapis Presents</h3>
+          <h1 className="bebas-neue-regular">FootballAPI</h1>
+        </div>
 
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          sint aspernatur temporibus, odio minima impedit veniam exercitationem
-          quidem, laborum, ab id ullam? Provident vero enim qui asperiores
-          perspiciatis vitae libero!
-        </p>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            sint aspernatur temporibus, odio minima impedit veniam
+            exercitationem quidem, laborum, ab id ullam? Provident vero enim qui
+            asperiores perspiciatis vitae libero!
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div id={styles.navCont}>
+      <div>
+        <img src="" alt="logo" />
+      </div>
+      <ul>
+        <li>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/docs">
+            <button>Docs</button>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
